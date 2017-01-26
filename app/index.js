103cd321ffe78a601790a070722718e29943c845
Antoine Colmard <a.colmard@gmail.com>
2017-01-27 00:38:24 +0100

    Enable CORS

--- app/index.js
+++ app/index.js
@@ -25,6 +25,12 @@ app.use(compression());
 app.use(morgan('dev', { stream: requestLogger.stream }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use('/api/names', names);
 app.use('/api/words', words);
 
