e6401efb2699bca2ee6a81977b53f1a1bdd23cf2
Antoine Colmard <a.colmard@gmail.com>
2017-01-27 21:04:31 +0100

    Remove json api implementation - KISS

--- app/names/index.js
+++ app/names/index.js
@@ -1,78 +1,29 @@
 const express = require('express');
-const names = require('./service');
-const url = require('url');
-const JSONAPISerializer = require('jsonapi-serializer').Serializer;
+const Name = require('./service');
 
 const router = new express.Router();
 
 router.get('/', (req, res) => {
-  const meta = {};
+  const query = {};
   if (undefined !== req.query.limit) {
-    meta.limit = parseInt(req.query.limit, 10);
+    query.$limit = parseInt(req.query.limit, 10);
   }
-  if (undefined !== req.query.offset) {
-    meta.offset = parseInt(req.query.offset, 10);
+  if (undefined !== req.query.skip) {
+    query.$skip = parseInt(req.query.skip, 10);
   }
   if (undefined !== req.query.seed) {
-    meta.seed = parseInt(req.query.seed, 10);
+    query.seed = parseInt(req.query.seed, 10);
   }
-  const collection = names.generateNames(meta.limit, meta.offset, meta.seed);
-
-  const links = {
-    self: url.format({
-      protocol: req.protocol,
-      host: req.get('host'),
-      pathname: url.parse(req.originalUrl).pathname,
-      query: meta,
-    }),
-    next: url.format({
-      protocol: req.protocol,
-      host: req.get('host'),
-      pathname: url.parse(req.originalUrl).pathname,
-      query: Object.assign({}, meta, { offset: meta.offset ? meta.offset + 1 : 1 }),
-    }),
-  };
-
-  if (meta.offset && meta.offset > 0) {
-    links.previous = url.format({
-      protocol: req.protocol,
-      host: req.get('host'),
-      pathname: url.parse(req.originalUrl).pathname,
-      query: Object.assign({}, meta, { offset: meta.offset - 1 }),
-    });
-  }
-
-  const NameSerializer = new JSONAPISerializer('names', {
-    topLevelLinks: links,
-    meta,
-    attributes: ['first', 'last'],
-  });
-
-  return res.json(NameSerializer.serialize(collection));
+  return res.json(Name.find(query));
 });
 
 router.get('/:id', (req, res) => {
-  const meta = {};
-  if (undefined !== req.query.seed) {
-    meta.seed = parseInt(req.query.seed, 10);
-  }
   const index = parseInt(req.params.id, 10);
-  const links = {
-    self: url.format({
-      protocol: req.protocol,
-      host: req.get('host'),
-      pathname: url.parse(req.originalUrl).pathname,
-      query: meta,
-    }),
-  };
-
-  const NameSerializer = new JSONAPISerializer('names', {
-    topLevelLinks: links,
-    meta,
-    attributes: ['first', 'last'],
-  });
-
-  return res.json(NameSerializer.serialize(names.generateName(index, meta.seed)));
+  const query = {};
+  if (undefined !== req.query.seed) {
+    query.seed = parseInt(req.query.seed, 10);
+  }
+  return res.json(Name.findById(index, query));
 });
 
 module.exports = router;
--- app/names/service.js
+++ app/names/service.js
@@ -1,17 +1,17 @@
 /* eslint-disable no-underscore-dangle */
 const fs = require('fs');
 const winston = require('winston');
-const syllables = require('../syllables/service');
-const words = require('../words/service');
+const Syllables = require('../syllables/service');
+const Words = require('../words/service');
 
 const logger = winston.loggers.get('words');
 const db = fs.readFileSync('./data/names.txt', 'utf-8').toString().split('\n');
 logger.info(`${db.length} names loaded`);
 
 const defaults = {
+  $limit: 10,
+  $skip: 0,
   seed: 1234,
-  offset: 0,
-  limit: 10,
 };
 
 function randomize(id, seed) {
@@ -24,26 +24,23 @@ function randomize(id, seed) {
  * @return {string} - the generated first name
  */
 function generateFirstName(lastName) {
-  const lastNameFirstSyllable = syllables.first(lastName);
-  return db.find(firstName => syllables.isSamePronunciation(
-    syllables.last(firstName),
+  const lastNameFirstSyllable = Syllables.first(lastName);
+  return db.find(firstName => Syllables.isSamePronunciation(
+    Syllables.last(firstName),
     lastNameFirstSyllable));
 }
 
 /**
  * Generate a punny name
  * @param {number} id - the name identifier
- * @param {number} seed - the generator seed
+ * @param {Object} query - the query parameters
+ * @param {number} query.seed - the generator seed
  * @return {Name} - the generated punny name
  */
-function _generateName(id, seed) {
-  const lastName = words.db[randomize(id, seed) % words.db.length];
+function generateName(id, query) {
+  const lastName = Words.db[randomize(id, query.seed) % Words.db.length];
   const firstName = generateFirstName(lastName);
-
-  if (undefined === firstName) {
-    words.db.splice(randomize(id, seed) % words.db.length, 1);
-  }
-  const lastNameSyllables = syllables.tokenize(lastName);
+  const lastNameSyllables = Syllables.tokenize(lastName);
   lastNameSyllables.shift();
   return {
     id,
@@ -55,42 +52,45 @@ function _generateName(id, seed) {
 /**
  * Generate a punny name
  * @param {number} id - the name identifier
- * @param {number} [seed=1234] - the generator seed
+ * @param {Object} [query] - the query parameters
+ * @param {number} [query.seed=1234] - the generator seed
  * @return {Name} - the generated punny name
  */
-function generateName(id, seed) {
-  return _generateName(id, seed || defaults.seed);
+function findById(id, query) {
+  return generateName(id, Object.assign({}, defaults, query));
 }
 
 /**
  * Generate a set of punny names
- * @param {number} limit - the number of names generated
- * @param {number} offset - the pagination offset
- * @param {number} seed - the generator seed
+ * @param {Object} query - the query parameters
+ * @param {number} query.$limit - the number of names generated
+ * @param {number} query.$skip - the pagination offset
+ * @param {number} query.seed - the generator seed
  * @return {Name[]} - the generated array of names
  */
-function _generateNames(limit, offset, seed) {
+function generateNames(query) {
   const names = [];
-  const from = (offset * limit) + 1;
-  const to = from + limit;
+  const from = query.$skip + 1;
+  const to = from + query.$limit;
   for (let id = from; id < to; id += 1) {
-    names.push(_generateName(id, seed));
+    names.push(generateName(id, query));
   }
   return names;
 }
 
 /**
  * Generate a set of punny names
- * @param {number} [limit=10] - the number of names generated
- * @param {number} [offset=0] - the pagination offset
- * @param {number} [seed=1234] - the generator seed
+ * @param {Object} query - the query parameters
+ * @param {number} [query.$limit=10] - the number of names generated
+ * @param {number} [query.$skip=0] - the pagination offset
+ * @param {number} [query.seed=1234] - the generator seed
  * @return {Name[]} - the generated array of names
  */
-function generateNames(limit, offset, seed) {
-  return _generateNames(limit || defaults.limit, offset || defaults.offset, seed || defaults.seed);
+function find(query) {
+  return generateNames(Object.assign({}, defaults, query));
 }
 
 module.exports = {
-  generateName,
-  generateNames,
+  findById,
+  find,
 };
