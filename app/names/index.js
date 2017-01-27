c0b82861602f3c1575d311442db4c275b9c8f740
Antoine Colmard <a.colmard@gmail.com>
2017-01-27 20:07:25 +0100

    Add json api implementation

--- app/names/index.js
+++ app/names/index.js
@@ -1,21 +1,78 @@
 const express = require('express');
 const names = require('./service');
+const url = require('url');
+const JSONAPISerializer = require('jsonapi-serializer').Serializer;
 
 const router = new express.Router();
 
 router.get('/', (req, res) => {
-  const seed = parseInt(req.query.seed, 10);
-  const offset = parseInt(req.query.offset, 10);
-  const limit = parseInt(req.query.limit, 10);
+  const meta = {};
+  if (undefined !== req.query.limit) {
+    meta.limit = parseInt(req.query.limit, 10);
+  }
+  if (undefined !== req.query.offset) {
+    meta.offset = parseInt(req.query.offset, 10);
+  }
+  if (undefined !== req.query.seed) {
+    meta.seed = parseInt(req.query.seed, 10);
+  }
+  const collection = names.generateNames(meta.limit, meta.offset, meta.seed);
 
-  return res.json(names.generateNames(limit, offset, seed));
+  const links = {
+    self: url.format({
+      protocol: req.protocol,
+      host: req.get('host'),
+      pathname: url.parse(req.originalUrl).pathname,
+      query: meta,
+    }),
+    next: url.format({
+      protocol: req.protocol,
+      host: req.get('host'),
+      pathname: url.parse(req.originalUrl).pathname,
+      query: Object.assign({}, meta, { offset: meta.offset ? meta.offset + 1 : 1 }),
+    }),
+  };
+
+  if (meta.offset && meta.offset > 0) {
+    links.previous = url.format({
+      protocol: req.protocol,
+      host: req.get('host'),
+      pathname: url.parse(req.originalUrl).pathname,
+      query: Object.assign({}, meta, { offset: meta.offset - 1 }),
+    });
+  }
+
+  const NameSerializer = new JSONAPISerializer('names', {
+    topLevelLinks: links,
+    meta,
+    attributes: ['first', 'last'],
+  });
+
+  return res.json(NameSerializer.serialize(collection));
 });
 
 router.get('/:id', (req, res) => {
+  const meta = {};
+  if (undefined !== req.query.seed) {
+    meta.seed = parseInt(req.query.seed, 10);
+  }
   const index = parseInt(req.params.id, 10);
-  const seed = parseInt(req.query.seed, 10);
+  const links = {
+    self: url.format({
+      protocol: req.protocol,
+      host: req.get('host'),
+      pathname: url.parse(req.originalUrl).pathname,
+      query: meta,
+    }),
+  };
 
-  return res.json(names.generateName(index, seed));
+  const NameSerializer = new JSONAPISerializer('names', {
+    topLevelLinks: links,
+    meta,
+    attributes: ['first', 'last'],
+  });
+
+  return res.json(NameSerializer.serialize(names.generateName(index, meta.seed)));
 });
 
 module.exports = router;
